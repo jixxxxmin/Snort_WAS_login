aff470efd78c39dfde4c484aee0ca36193dd9449
Min <112598226+jixxxxmin@users.noreply.github.com>
2025-08-05 19:59:13 +0900

    a

--- login/src/LoginServlet.java
+++ login/src/LoginServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.http.*;
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+                
+                request.setCharacterEncoding("UTF-8");
+                response.setContentType("text/plain;charset=UTF-8");
+                response.setCharacterEncoding("UTF-8"); 
 
                 String id = request.getParameter("id");
                 String pw = request.getParameter("password");
