c1ca07ca0c34f74b30e3548a1025616e574222ee
Min <112598226+jixxxxmin@users.noreply.github.com>
2025-08-12 16:27:28 +0900

    Update LoginServlet.java

--- login/src/LoginServlet.java
+++ login/src/LoginServlet.java
@@ -36,7 +36,7 @@ public class LoginServlet extends HttpServlet {
                     session.setMaxInactiveInterval(3600);
                     session.setAttribute("id", id);
                         
-                    response.sendRedirect("/adminauth");
+                    response.sendRedirect("/admin");
                 }
                 else {
                     HttpSession oldSession = request.getSession(false);
